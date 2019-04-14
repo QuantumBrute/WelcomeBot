f74477829a290f6fd0d274b36e6fa39e4cebc721
QuantumBrute <28859415+QuantumBrute@users.noreply.github.com>
2019-04-15 01:30:52 +0530

    Add files via upload

---
+++ WelcomeBot.py
@@ -0,0 +1,107 @@
+import praw
+import LoginInfo
+import json
+import time
+from psaw import PushshiftAPI
+#Created by u/QuantumBrute
+api = PushshiftAPI()     
+data = []
+itemname = []
+
+print ("Starting Bot...")
+print ("Logging in...")
+
+#To login into reddit
+reddit = praw.Reddit(client_id= LoginInfo.client_id,
+					 client_secret= LoginInfo.client_secret,
+					 username= LoginInfo.username,
+					 password= LoginInfo.password,
+					 user_agent= LoginInfo.user_agent)
+
+print ("Logged in!")
+
+print("Opening LastApprovedUser...")
+with open('LastApprovedUser.txt', 'r') as infile:
+    info = json.load(infile,)
+
+subreddit = reddit.subreddit("TheApexCollective") # Defines which subreddit the bot should work on
+
+print("Looking for new and unflaired users...")
+
+def unflaired():
+    n = len(info) + 1
+    m = info[n-2].get("Number")
+    mnew1 = m + 1
+    #mnew2 = 0
+    #mnew3 = 0
+    print("Current number of existing users: " + str(m))
+    #flag = 0
+    #check = 0
+    checknew = 0
+    for contributor1 in subreddit.contributor():
+        for flair in subreddit.flair(redditor=contributor1, limit=1):
+            if flair.get("flair_text") == None:
+                print("Unflaired user found!")
+                print(str(contributor1) + " will be flaired: Mortal ("  + str(mnew1) + "th)")
+                newflair = ("Mortal (" + str(mnew1) + "th)")
+                subreddit.flair.set(contributor1,newflair)
+                itemfinal = { "Name" : str(contributor1), "Number" : mnew1}
+                data.append(itemfinal)
+                with open('LastApprovedUser.txt', 'w') as outfile:
+                    json.dump(data, outfile, indent=2)
+                checknew = 1
+                print("User appended successfully!")
+                print("New number of existing users: " + str(mnew1))
+                mnew1 = mnew1 + 1
+            else:
+                checknew = 0
+                break
+        
+    if checknew == 0:
+        print("No new users found!")
+
+    '''if n==1:
+        for contributor2 in subreddit.contributor():
+            itemname.append(str(contributor2))
+        itemname.reverse()
+        for x in range(len(itemname)):
+            mnew2 = m + 1
+            itemfinal = { "Name" : itemname[x], "Number" : mnew2}
+            data.append(itemfinal)
+            with open('LastApprovedUser.txt', 'w') as outfile:
+                json.dump(data, outfile, indent=2)
+        print("First Time!")
+        print("Data added successfully")
+        n = 0
+    else:
+        for y in range(len(info)):
+            for contributor3 in subreddit.contributor():
+                if contributor3 == info[y].get("Name"):
+                    print("Users already in list!")
+                    flag = 0
+                    check = 1
+                    break
+                else:
+                    flag = 1
+            if flag == 1:
+                itemname.append(str(contributor3))
+            if check == 1:
+                break
+        if flag == 1:
+            itemname.reverse()
+            for x in range(len(itemname)):
+                mnew3 = m + 1
+                itemfinal = { "Name" : itemname[x], "Number" : mnew3}
+                data.append(itemfinal)
+                print("User Appended!")
+                with open('LastApprovedUser.txt', 'w') as outfile:
+                    json.dump(data, outfile, indent=2)
+                print("Data added successfully")
+        else:
+            print("No data to add!")'''
+   
+while True:
+    unflaired()
+    print("Sleeping for 30 minutues!")
+    time.sleep(60*30)
+
